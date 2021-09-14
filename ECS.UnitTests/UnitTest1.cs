a6edf122f3c0ade025a023ce158ec66cbef2ef0f
phil2988 <phillipblicher@hotmail.com>
2021-09-14 09:48:15 +0100

    TestLogger

--- ECS.UnitTests/UnitTest1.cs
+++ ECS.UnitTests/UnitTest1.cs
@@ -4,11 +4,16 @@ namespace ECS.UnitTests
 {
     public class Tests
     {
+
         [SetUp]
         public void Setup()
         {
+
+            //Heater Setup
+
         }
 
+        //Heater Tests
         [Test]
         public void Test1()
         {
